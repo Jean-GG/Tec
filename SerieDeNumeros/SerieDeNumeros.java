19866202079fd0b7544f9d3e54c092b1a65ca12e
Jean-GG <jcgaribayg02@hotmail.com>
2021-09-16 16:21:24 -0500

    Updated

---
+++ SerieDeNumeros/SerieDeNumeros.java
@@ -0,0 +1,20 @@
+public class SerieDeNumeros
+{
+    public static void main(String[] args) throws Exception {
+        int valor1 = 0;
+        int valor2 = 1;
+        int ciclos = 10;
+        int suma  = 0;
+
+        //System.out.println(valor1);
+        //System.out.println(valor2);
+
+        for(int i= 0; i < ciclos; i = i + 1)
+        {
+            suma = valor1 + valor2;
+            System.out.println(suma);
+            valor1 = valor2; 
+            valor2 = suma;    
+        }
+    }
+}
